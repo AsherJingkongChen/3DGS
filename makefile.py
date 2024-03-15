7d25d09e8dfe3f81a3d821f87163d19ab4c5a7a8
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2024-03-15 18:24:20 +0800

    (build-fix): Add remove step

--- makefile.py
+++ makefile.py
@@ -7,7 +7,9 @@ Prerequisites:
 from pathlib import Path
 from subprocess import run
 
+old_image_paths = Path.cwd().glob("**/*-*.svg")
 original_markdown_paths = Path.cwd().glob("**/*.orig.md")
+list(map(Path.unlink, old_image_paths))
 
 for orig_path in original_markdown_paths:
     new_path = orig_path.parent / Path(orig_path.stem).with_suffix(".md")
